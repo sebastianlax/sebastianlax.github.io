16be387afee898198be7ada451f02176b26dc9e3
sebastianlax <106210273+sebastianlax@users.noreply.github.com>
2023-04-02 16:57:58 -0300

    Update contacto.js

--- contacto.js
+++ contacto.js
@@ -5,7 +5,7 @@ $("#contacto").submit(function(event) {
     var apellido = $("#apellido").val(); 
     var email = $("#email").val(); 
     var mensaje = $("#mensaje").val();    
-    var mensajeWPP = "Nombre: " + nombre + "\n Apellido:" + apellido + "\n Email" + email + "\n Mensaje" + mensaje; 
+    var mensajeWPP = "Nombre: " + nombre + "\n Apellido: " + apellido + "\n Email: " + email + "\n Mensaje: " + mensaje; 
     var miNumero = "+5491162875482";
     window.open(`https://wa.me/${miNumero}?text=${mensajeWPP}`, "_blank")
 
